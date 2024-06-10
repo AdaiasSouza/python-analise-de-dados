f14838c3cacf3ef40488aa6b7cc95773d7c62aee
AdaiasSouza <adaias_305@hotmail.com>
2024-06-10 11:38:48 -0300

    Resolucao da primeira lista

---
+++ lista_01/exercicio7.py
@@ -0,0 +1,10 @@
+# Exercício 7 - Adicione mais um elemento ao dicionário criado no exercício anterior e imprima na tela
+dias_semana = {
+	1: "Domingo",
+	2: "Segunda Feira",
+	3: "Terça Feira"
+}
+
+dias_semana[4] = "Quarta Feira"
+
+print(dias_semana)
---
+++ lista_01/exercicio9.py
@@ -0,0 +1,7 @@
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string, 
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e 
+# o quarto elemento um valor do tipo float.
+# Imprima a lista.
+
+list_element = ["Python", (1, 2), {1: "Java", 2: "Delphin"}, 2.3]
+print(list_element)
