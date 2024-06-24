a28a0c95478f44647e17ce055037692daa76054a
AdaiasSouza <adaias_305@hotmail.com>
2024-06-24 10:41:46 -0300

    atualizando lista04

--- lista_04/exercicio2.py
+++ lista_04/exercicio2.py
@@ -1,21 +1,35 @@
-# Exercício 2 - Crie uma classe chamada Pessoa() com os atributos: nome, cidade, telefone e e-mail. Use pelo menos 2
-# métodos especiais na sua classe. Crie um objeto da sua classe e faça uma chamada a pelo menos um dos seus métodos
-# especiais.
+"""
+ Exercício 2
+    Crie uma classe chamada Pessoa() com os atributos:
+        nome, cidade, telefone e e-mail.
+    Use pelo menos 2  métodos especiais na sua classe.
+    Crie um objeto da sua classe
+    e faça uma chamada a pelo menos um dos seus métodos
+    especiais.
+"""
+
+
 class Pessoa():
-	"""docstring for Pessoa"""	
-	def __init__(self, nome, cidade, telefone, e_mail):		
-		self.nome = nome
-		self.cidade = cidade
-		self.telefone = telefone
-		self.e_mail = e_mail		
+    """docstring for Pessoa"""
 
-	def imprime_pessoa(self):
-		print(f"Nome: {self.nome} Cidade: {self.cidade} Telefone: {self.telefone} E-mail: {self.e_mail}")
+    def __init__(self, nome, cidade, telefone, e_mail):
+        self.nome = nome
+        self.cidade = cidade
+        self.telefone = telefone
+        self.e_mail = e_mail
 
-	def troca_telefone(self, novo_telefone):
-		fone_antigo = self.telefone
-		self.telefone = novo_telefone
-		print(f"Mudança de telefone: {fone_antigo} -> {self.telefone}")		
+    def imprime_pessoa(self):
+
+        # print(f"Nome: {self.nome} Cidade: {self.cidade} \
+        #             Telefone: {self.telefone} \
+        #                 E-mail: {self.e_mail}")
+        msg_string = "Nome: {%s}".format(self.nome)
+        print(msg_string)
+
+    def troca_telefone(self, novo_telefone):
+        fone_antigo = self.telefone
+        self.telefone = novo_telefone
+        print(f"Mudança de telefone: {fone_antigo} -> {self.telefone}")
 
 
 # Instanciando Classes
@@ -28,3 +42,4 @@ p2 = Pessoa('Amora', 'Fortaleza', '32335566', 'amora_love@gmail.com')
 p2.imprime_pessoa()
 p2.troca_telefone('34965454')
 p2.imprime_pessoa()
+print()
