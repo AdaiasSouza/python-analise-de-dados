80ba6386ffd0e420e27e3258c043655120e530d7
Adaias Alves <59511536+AdaiasSouza@users.noreply.github.com>
2024-07-09 13:48:20 -0300

    Delete lista_03 directory
    fora de uso

--- lista_03/exercicio1.py
+++
@@ -1,8 +0,0 @@
-# Exercício 1 - Crie uma lista de 3 elementos e calcule a terceira potência de cada elemento.
-list_elementos = [1, 2, 3]
-index = 0
-for valor in list_elementos:
-	valor_potencia = pow(valor, 3)
-	list_elementos[index] = valor_potencia
-	index += 1
-print(list_elementos)
